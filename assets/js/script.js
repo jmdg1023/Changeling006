c909d4b2e516347de7d4ecefa169225a1cbe17e9
Astrid <jmdg1023@gmail.com>
2022-09-22 18:21:01 +1000

    coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,30 +18,7 @@ function searchCity(event) {
     inputEl.val("");
 
     storeSearchList();
-}
-
-function latLong(query) {
-    currentForecast.html('');
-    futureForecast.html('');
-    fetch(`https://api.openweathermap.org/geo/1.0/direct?appid=${apiKey}&limit=1&q=` + query)
-        .then(
-            function (response) {
-                return response.json();
-            },
-            function (error) {
-                console.log(error.message);
-            }
-        )
-        .then(function (data) {
-            console.log(data);
-            var lat = data[0].lat
-            var long = data[0].long
-
-            weatherApi(lat, long);
-            fiveDayWeatherApi(lat, long);
-
-        });
-    inputEl.val("");
+    renderSearchList();
 }
 
 // This function is being called below and will run when the page loads.
@@ -78,82 +55,113 @@ function renderSearchList() {
         });
 
         recentSearchButton.text(search).appendTo(searchListContainer);
+
+        $(recentSearchButton).on("click", function (event) {
+            searchItem = $(event.target).text();
+            console.log(searchItem);
+
+            latLong(searchItem);
+        }
+}
+
+    searchButtonEl.on("click", searchCity);
+
+    init();
+
+    function latLong(query) {
+        currentForecast.html('');
+        futureForecast.html('');
+        fetch(`https://api.openweathermap.org/geo/1.0/direct?appid=${apiKey}&limit=1&q=` + query)
+            .then(
+                function (response) {
+                    return response.json();
+                },
+                function (error) {
+                    console.log(error.message);
+                }
+            )
+            .then(function (data) {
+                console.log(data);
+                var lat = data[0].lat
+                var lon = data[0].lon
+
+                fetchWeather(lat, lon);
+                futureForecastApi(lat, lon);
+
+            });
+        inputEl.val("");
     }
-}
 
-searchButtonEl.on("click", searchCity);
-
-init();
-
-
-function fetchWeather() {
-    var weather =
-        "https://api.openweathermap.org/data/2.5/uvi?lat=" +
-        latitude +
-        "&lon=" +
-        longitude +
-        "&appid=" +
-        APIKey;
-    //api.openweathermap.org / data / 2.5 / forecast ? lat = { lat } & lon={ lon }& appid={ 6b9b01d3d9f1c0c20437677fc0595010 };
-    fetch(weather)
-        .then(function (response) {
-            return response.json();
-        })
-        .then((data) => {
-            console.log(data);
-        });
-
-
-}
-
-function futureForecastApi(lat, lon) {
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&cnt=5&units=metric` + `&lat=` + lat + '&lon=' + lon)
-        .then(
-            function (response) {
+    function fetchWeather() {
+        var weather =
+            "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+            latitude +
+            "&lon=" +
+            longitude +
+            "&appid=" +
+            APIKey;
+        //api.openweathermap.org / data / 2.5 / forecast ? lat = { lat } & lon={ lon }& appid={ 6b9b01d3d9f1c0c20437677fc0595010 };
+        fetch(weather)
+            .then(function (response) {
                 return response.json();
-            },
-            function (error) {
-                console.log(error.message);
-            }
-        )
-        .then(function (data) {
-            console.log(data);
-            appendFutureForecast(data);
-        });
-}
-
-function appendCurrentForcast(data) {
-
-    var currentDate = moment().format('dddd, Do MMMM YYYY')
-    var cityLoc = $(`<h2 class="text-xl font-bold mb-1">${data.name} - ${currentDate} <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></h2>`)
-    var descriptionEl = data.weather[0].description
-    var description = descriptionEl.toUpperCase()
+            })
+            .then((data) => {
+                console.log(data){
+                    appendCurrentForcast(data)
+                };
+            });
 
 
-    todayForecast.append(cityLoc)
-    todayForecast.append(`<p class='mt-px mb-8'> ${description}`)
-    todayForecast.append(`<p class="mb-4">Temp: ${data.main.temp} °C</p>`)
-    todayForecast.append(`<p class="mb-4">Wind: ${data.wind.speed} KM/H</p>`)
-    todayForecast.append(`<p class="mb-4">Humidity: ${data.main.humidity}%</p>`)
-    // todayForecast.append(`<p class="mb-4">UV Index: ${data.wind.speed}</p>`)
-}
-
-function appendFutureForecast(data) {
-
-    fiveDayContainer.append('<h3 class="text-xl font-bold mb-8 col-span-10">5-Day Forecast:</h3>')
-
-    for (var i = 0; i < data.list.length; i++) {
-        var list = data.list[i]
-        var cardContainer = $('<div class="col-span-10 md:col-span-2 bg-indigo-400 text-white p-3 rounded-lg m-1">');
-        let date = moment().add(i + 1, 'days').format('DD/MM/YYYY');
-        fiveDayContainer.append(cardContainer);
-        cardContainer.append(`<p class="text-xl font-bold mb-4">${date}</p>`);
-        cardContainer.append(`<img src="http://openweathermap.org/img/wn/${list.weather[0].icon}@2x.png">`);
-        cardContainer.append(`<p class="mb-4">Temp: ${list.main.temp} °C</p>`);
-        cardContainer.append(`<p class="mb-4">Wind: ${list.wind.speed}KM/H</p>`);
-        cardContainer.append(`<p class="mb-4">Humidity: ${list.main.humidity}%</p>`);
     }
 
-}
+    function futureForecastApi(lat, lon) {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&cnt=5&units=metric` + `&lat=` + lat + '&lon=' + lon)
+            .then(
+                function (response) {
+                    return response.json();
+                },
+                function (error) {
+                    console.log(error.message);
+                }
+            )
+            .then(function (data) {
+                console.log(data);
+                appendFutureForecast(data);
+            });
+    }
+
+    function appendCurrentForcast(data) {
+
+        var currentDate = moment().format('dddd, Do MMMM YYYY')
+        var cityLoc = $(`<h2 class="text-xl font-bold mb-1">${data.name} - ${currentDate} <img src="http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"></h2>`)
+        var descriptionEl = data.weather[0].description
+        var description = descriptionEl.toUpperCase()
+
+
+        todayForecast.append(cityLoc)
+        todayForecast.append(`<p class='mt-px mb-8'> ${description}`)
+        todayForecast.append(`<p class="mb-4">Temp: ${data.main.temp} °C</p>`)
+        todayForecast.append(`<p class="mb-4">Wind: ${data.wind.speed} KM/H</p>`)
+        todayForecast.append(`<p class="mb-4">Humidity: ${data.main.humidity}%</p>`)
+        // todayForecast.append(`<p class="mb-4">UV Index: ${data.wind.speed}</p>`)
+    }
+
+    function appendFutureForecast(data) {
+
+        fiveDayContainer.append('<h3 class="text-xl font-bold mb-8 col-span-10">5-Day Forecast:</h3>')
+
+        for (var i = 0; i < data.list.length; i++) {
+            var list = data.list[i]
+            var cardContainer = $('<div class="col-span-10 md:col-span-2 bg-indigo-400 text-white p-3 rounded-lg m-1">');
+            let date = moment().add(i + 1, 'days').format('DD/MM/YYYY');
+            fiveDayContainer.append(cardContainer);
+            cardContainer.append(`<p class="text-xl font-bold mb-4">${date}</p>`);
+            cardContainer.append(`<img src="http://openweathermap.org/img/wn/${list.weather[0].icon}@2x.png">`);
+            cardContainer.append(`<p class="mb-4">Temp: ${list.main.temp} °C</p>`);
+            cardContainer.append(`<p class="mb-4">Wind: ${list.wind.speed}KM/H</p>`);
+            cardContainer.append(`<p class="mb-4">Humidity: ${list.main.humidity}%</p>`);
+        }
+
+    }
 
 
